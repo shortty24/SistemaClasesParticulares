1311e0e8beb741c2b0a4c3f22eaaf6b2a71c363e
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-05-09 23:16:39 -0500

    Implementé la clase Profesor.cpp

--- SistemaClasesParticularesModel/Profesor.cpp
+++ SistemaClasesParticularesModel/Profesor.cpp
@@ -1,13 +1,12 @@
 #include "Profesor.h"
 
 Profesor::Profesor(int codigo, int calificacion, int horario, int precio, CV^ objCV, List<Curso^>^ objListaCursos) {
-	/*
 	this->codigo = codigo;
 	this->calificacion = calificacion;
 	this->horario = horario;
 	this->precio = precio;
 	this->objCV = objCV;
 	this->objListaCursos = objListaCursos;
-	*/
+	
 
 }
