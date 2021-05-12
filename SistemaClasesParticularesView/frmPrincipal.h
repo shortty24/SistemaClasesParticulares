086e100b56ab048b1b856f94faad7a8689b8e6a4
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-05-11 20:09:56 -0500

    Aguegué botones en la pantalla de diseño de la clase frmPrincipal

--- SistemaClasesParticularesView/frmPrincipal.h
+++ SistemaClasesParticularesView/frmPrincipal.h
@@ -34,6 +34,10 @@ namespace SistemaClasesParticularesView {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+
 
 	private:
 		/// <summary>
@@ -48,12 +52,51 @@ namespace SistemaClasesParticularesView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"frmPrincipal";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(224, 228);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(118, 46);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Iniciar Sesion";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &frmPrincipal::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(348, 228);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(118, 46);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Registrate";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &frmPrincipal::button2_Click);
+			// 
+			// frmPrincipal
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(723, 500);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"frmPrincipal";
+			this->Text = L"frmPrincipal";
+			this->Load += gcnew System::EventHandler(this, &frmPrincipal::frmPrincipal_Load);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void frmPrincipal_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
