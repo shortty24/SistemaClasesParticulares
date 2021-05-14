d01709ac6b89ff694a5654ddac69dd79e4ddd4e4
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-05-13 20:06:36 -0500

    Modifiqué el diseño de frmPrincipal.h y agregué las ventanas frmRegistroProfesor.h y frmRegistroAlumno.h

--- SistemaClasesParticularesView/frmPrincipal.h
+++ SistemaClasesParticularesView/frmPrincipal.h
@@ -37,6 +37,7 @@ namespace SistemaClasesParticularesView {
 	private: System::Windows::Forms::Button^ button1;
 	protected:
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
 
 
 	private:
@@ -54,11 +55,12 @@ namespace SistemaClasesParticularesView {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(224, 228);
+			this->button1->Location = System::Drawing::Point(163, 228);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(118, 46);
 			this->button1->TabIndex = 0;
@@ -68,19 +70,29 @@ namespace SistemaClasesParticularesView {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(348, 228);
+			this->button2->Location = System::Drawing::Point(300, 228);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(118, 46);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"Registrate";
+			this->button2->Text = L"Registrate como estudiante";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &frmPrincipal::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(433, 228);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(118, 46);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Registrate como profesor";
+			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// frmPrincipal
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(723, 500);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"frmPrincipal";
---
+++ SistemaClasesParticularesView/frmRegistroProfesor.cpp
@@ -0,0 +1,2 @@
+#include "frmRegistroProfesor.h"
+
