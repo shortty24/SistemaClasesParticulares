65f1fed619f1175c74930d78dea084d9c3e419cc
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-05-17 20:29:42 -0500

    Modifiqué el diseño de la ventana de frmRegistroProfesor.h

--- SistemaClasesParticularesView/frmRegistroProfesor.h
+++ SistemaClasesParticularesView/frmRegistroProfesor.h
@@ -34,6 +34,23 @@ namespace SistemaClasesParticularesView {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button2;
 
 	private:
 		/// <summary>
@@ -48,18 +65,184 @@ namespace SistemaClasesParticularesView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(298, 353);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(130, 23);
+			this->button1->TabIndex = 29;
+			this->button1->Text = L"Registrarse";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(273, 287);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(61, 13);
+			this->label7->TabIndex = 28;
+			this->label7->Text = L"Contraseña";
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(340, 284);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(130, 20);
+			this->textBox7->TabIndex = 27;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(295, 261);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(38, 13);
+			this->label6->TabIndex = 26;
+			this->label6->Text = L"Correo";
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(340, 258);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(130, 20);
+			this->textBox6->TabIndex = 25;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(248, 235);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(86, 13);
+			this->label5->TabIndex = 24;
+			this->label5->Text = L"Apellido Materno";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(250, 209);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(84, 13);
+			this->label4->TabIndex = 23;
+			this->label4->Text = L"Apellido Paterno";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(290, 183);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(44, 13);
+			this->label3->TabIndex = 22;
+			this->label3->Text = L"Nombre";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(307, 157);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(26, 13);
+			this->label2->TabIndex = 21;
+			this->label2->Text = L"DNI";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(290, 131);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(43, 13);
+			this->label1->TabIndex = 20;
+			this->label1->Text = L"Usuario";
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(340, 232);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(130, 20);
+			this->textBox5->TabIndex = 19;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(340, 206);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(130, 20);
+			this->textBox4->TabIndex = 18;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(340, 180);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(130, 20);
+			this->textBox3->TabIndex = 17;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(340, 154);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(130, 20);
+			this->textBox2->TabIndex = 16;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(340, 128);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(130, 20);
+			this->textBox1->TabIndex = 15;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(340, 310);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(130, 23);
+			this->button2->TabIndex = 30;
+			this->button2->Text = L"Adjuntar CV";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &frmRegistroProfesor::button2_Click);
 			// 
 			// frmRegistroProfesor
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(718, 477);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Name = L"frmRegistroProfesor";
 			this->Text = L"frmRegistroProfesor";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
