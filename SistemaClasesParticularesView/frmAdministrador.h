ef1d7bd043d582d13b7ebe7441e276731ad4a9e6
Hugo <Hugo@DESKTOP-HQRGQCE>
2021-05-20 17:10:40 -0500

    Modicación de Ventana

--- SistemaClasesParticularesView/frmAdministrador.h
+++ SistemaClasesParticularesView/frmAdministrador.h
@@ -89,7 +89,7 @@ namespace SistemaClasesParticularesView {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(545, 273);
+			this->ClientSize = System::Drawing::Size(567, 297);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
