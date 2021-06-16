394f6043527e33322d5f56e5e4ceee602020b776
Hugo <Hugo@DESKTOP-HQRGQCE>
2021-06-16 18:52:05 -0500

    Cambios en programar clase

--- SistemaClasesParticularesView/frmProgramarClase.h
+++ SistemaClasesParticularesView/frmProgramarClase.h
@@ -40,12 +40,34 @@ namespace SistemaClasesParticularesView {
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
+
+
+
+
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column4;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column2;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column3;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Column5;
-	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -70,14 +92,20 @@ namespace SistemaClasesParticularesView {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Column5 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->Column5 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Column4 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->groupBox1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->groupBox2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// groupBox1
@@ -90,7 +118,7 @@ namespace SistemaClasesParticularesView {
 			this->groupBox1->Size = System::Drawing::Size(514, 72);
 			this->groupBox1->TabIndex = 3;
 			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Agregar Link";
+			this->groupBox1->Text = L"Clases Faltantes";
 			// 
 			// button1
 			// 
@@ -98,7 +126,7 @@ namespace SistemaClasesParticularesView {
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 2;
-			this->button1->Text = L"Añadir";
+			this->button1->Text = L"Buscar";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// textBox1
@@ -125,51 +153,100 @@ namespace SistemaClasesParticularesView {
 				this->Column4,
 					this->Column1, this->Column2, this->Column3, this->Column5
 			});
-			this->dataGridView1->Location = System::Drawing::Point(24, 110);
+			this->dataGridView1->Location = System::Drawing::Point(34, 114);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(514, 150);
+			this->dataGridView1->Size = System::Drawing::Size(555, 142);
 			this->dataGridView1->TabIndex = 2;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &frmProgramarClase::dataGridView1_CellContentClick);
 			// 
-			// Column4
+			// groupBox2
 			// 
-			this->Column4->HeaderText = L"Código Pago";
-			this->Column4->Name = L"Column4";
+			this->groupBox2->Controls->Add(this->button2);
+			this->groupBox2->Controls->Add(this->textBox2);
+			this->groupBox2->Controls->Add(this->label2);
+			this->groupBox2->Location = System::Drawing::Point(34, 285);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(514, 72);
+			this->groupBox2->TabIndex = 4;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Agregar Link";
 			// 
-			// Column1
+			// textBox2
 			// 
-			this->Column1->HeaderText = L"DNI de Alumno";
-			this->Column1->Name = L"Column1";
+			this->textBox2->Location = System::Drawing::Point(117, 26);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(187, 20);
+			this->textBox2->TabIndex = 1;
 			// 
-			// Column2
+			// label2
 			// 
-			this->Column2->HeaderText = L"DNI Profesor";
-			this->Column2->Name = L"Column2";
-			// 
-			// Column3
-			// 
-			this->Column3->HeaderText = L"Código de curso";
-			this->Column3->Name = L"Column3";
-			// 
-			// Column5
-			// 
-			this->Column5->HeaderText = L"Link";
-			this->Column5->Name = L"Column5";
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(32, 29);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(27, 13);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Link";
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(249, 291);
+			this->button2->Location = System::Drawing::Point(352, 26);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
-			this->button2->TabIndex = 4;
+			this->button2->TabIndex = 5;
 			this->button2->Text = L"Enviar Clase";
 			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(598, 52);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"Validar";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(706, 52);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 7;
+			this->button4->Text = L"No Validado";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// Column5
+			// 
+			this->Column5->HeaderText = L"Estado";
+			this->Column5->Name = L"Column5";
+			// 
+			// Column3
+			// 
+			this->Column3->HeaderText = L"Duracion";
+			this->Column3->Name = L"Column3";
+			// 
+			// Column2
+			// 
+			this->Column2->HeaderText = L"Hora Inicio";
+			this->Column2->Name = L"Column2";
+			// 
+			// Column1
+			// 
+			this->Column1->HeaderText = L"DNI alumno";
+			this->Column1->Name = L"Column1";
+			// 
+			// Column4
+			// 
+			this->Column4->HeaderText = L"Codigo de Pago";
+			this->Column4->Name = L"Column4";
 			// 
 			// frmProgramarClase
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(583, 365);
-			this->Controls->Add(this->button2);
+			this->ClientSize = System::Drawing::Size(822, 385);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->dataGridView1);
 			this->Name = L"frmProgramarClase";
@@ -177,11 +254,15 @@ namespace SistemaClasesParticularesView {
 			this->groupBox1->ResumeLayout(false);
 			this->groupBox1->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
+private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
 };
 }
