e66ec61c58699720381dc10487fcd9bc84eaadad
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-06-27 15:03:55 -0500

    Implementé el procesamiento de datos usando Base de Datos para el login y registro.

--- SistemaClasesParticularesController/LogeoController.cpp
+++ SistemaClasesParticularesController/LogeoController.cpp
@@ -4,107 +4,95 @@ using namespace System;
 using namespace SistemaClasesParticularesController;
 using namespace System::IO;
 
-
 LogeoController::LogeoController() {
 
 	//this->listaPersonas = gcnew List<Persona^>();
 	this->Existe = Existe;
 	this->Estado = Estado;
-	this->Verificacion = Verificacion;
-	//this->usuarioAlumno = usuarioAlumno;
+	this->EstadoCV = EstadoCV;
+	this->EstadoUsuario = EstadoUsuario;
+	this->objConexion = gcnew SqlConnection();
 }
-
+void LogeoController::AbrirConexion() {
+	this->objConexion->ConnectionString = "Server=200.16.7.140;DataBase=a20165855;User ID=a20165855;Password=h7b3EJcM;";
+	this->objConexion->Open(); 
+}
+void LogeoController::CerrarConexion() {
+	this->objConexion->Close();
+}
 int LogeoController::VerificarSiUsuarioExiste(String^ textBox1, String^ textBox2) {
-	array<String^>^ lineas = File::ReadAllLines("Personas.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaPersona in lineas) {
-		array<String^>^ palabras = lineaPersona->Split(separadores->ToCharArray());
-		String^ ID = palabras[0];
-		String^ dni = palabras[1];
-		String^ Usuario = palabras[2];
-		String^ Contrasenha = palabras[3];
-		String^ ApellidoPaterno = palabras[4];
-		String^ ApellidoMaterno = palabras[5];
-		String^ Nombre = palabras[6];
-		//Persona^ objPersona = gcnew Persona(dni, Usuario, Contrasenha, ApellidoPaterno, ApellidoMaterno, Nombre);
-		//this->listaPersonas->Add(objPersona);
-		if (textBox1 == Usuario && textBox2 == Contrasenha) {
-			if (ID == "A") {
-				if (VerificarEstadoUsuario(dni) == "No Bloqueado") {
-					Existe = 1;
-				}
-				else {
-					Existe = 6;
-				}
-			//usuarioAlumno = textBox1;
+	AbrirConexion();
+	SqlCommand^ objQuery = gcnew SqlCommand();
+	objQuery->Connection = this->objConexion;
+	objQuery->CommandText = "select * from Personas where Usuario='" + textBox1 + "' and Contrasenha='" + textBox2 + "';";
+	SqlDataReader^ objData= objQuery->ExecuteReader();
+	//String^ DNI = safe_cast<String^>(objData["DNI"]);
+	if (objData->Read()) {
+		String^ TipoUser = safe_cast<String^>(objData["TipoUsuario"]);
+		String^ Dni = safe_cast<String^>(objData["DNI"]);
+		objData->Close();
+		if (TipoUser == "A") {
+			EstadoUsuario = VerificarEstadoUsuario(Dni);
+			if (EstadoUsuario == "No Bloqueado") {
+				Existe = 1; // Alumno sí está en base de datos y no está bloqueado
 			}
-			else if (ID == "P") {
-				if (VerificarValidacionCV(dni) == "PorVerificar") {
-					Existe = 4;
-				}
-				else if (VerificarValidacionCV(dni) == "Desaprobado") {
-					Existe = 5;
-				}
-				else if (VerificarEstadoUsuario(dni) =="Bloqueado" ) {
-					Existe = 6;
-				}
-				else if(VerificarValidacionCV(dni)=="Aprobado" && VerificarEstadoUsuario(dni)=="No Bloqueado") {
-					Existe = 2;
-				}
-				
-
-
+			else {
+				Existe = 6; // Alumno sí está en base de datos, pero está bloqueado
 			}
-			else if (ID == "M") {
-				Existe = 3;
+		}
+		else if (TipoUser == "P") {
+			String^ EstadoCV = VerificarValidacionCV(Dni);
+			if (EstadoCV == "PorVerificar") {
+				Existe = 4;
 			}
-			 break;
+			else if (EstadoCV == "Desaprobado") {
+				Existe = 5;
+			}
+			else if (EstadoCV == "Bloqueado") {
+				Existe = 6;
+			}
+			else if (EstadoCV == "Aprobado" && EstadoUsuario == "No Bloqueado") {
+				Existe = 2;
+			}
+		}
+		else if (TipoUser == "M") {
+			Existe = 3;
 		}
 		else {
-			 Existe = 0;
-			
+			Existe = 0;
 		}
 	}
+	//objData->Close();
+	CerrarConexion();
 	return Existe;
 }
-
-/*String^ LogeoController::obtenerUsuario() {
-	return usuarioAlumno;
-}*/
-
-
 String^ LogeoController::VerificarValidacionCV(String^ DNI) {
-	array<String^>^ lineas = File::ReadAllLines("CVs.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaCV in lineas) {
-		array<String^>^ palabras = lineaCV->Split(separadores->ToCharArray());
-		//String^ ID = palabras[0];
-		String^ dniProfesor = palabras[0];
-		String^ CodigoMinedu = palabras[1];
-		String^ Empresa = palabras[2];
-		String^ TelefonoEmpresa = palabras[3];
-		String^ Validacion = palabras[4];
-		if (DNI== dniProfesor) {
-			Verificacion = Validacion;
-			break;
-		}
+
+	SqlCommand^ objQuery1 = gcnew SqlCommand();
+	objQuery1->Connection = this->objConexion;
+	objQuery1->CommandText = "select * from CVs where DNI='" + DNI + "';";
+	SqlDataReader^ objData1 = objQuery1->ExecuteReader();
+	if (objData1->Read()) {
+			EstadoCV = safe_cast<String^>(objData1["EstadoCV"]);
 	}
-	return Verificacion;
+	else {
+		EstadoCV = "";
+	}
+	objData1->Close();
+	return EstadoCV;
 }
 String^ LogeoController::VerificarEstadoUsuario(String^ DNI) {
-	array<String^>^ lineas = File::ReadAllLines("Quejas.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaQueja in lineas) {
-		array<String^>^ palabras = lineaQueja->Split(separadores->ToCharArray());
-		String^ dniAgresor = palabras[0];
-		String^ EstadoUsuario = palabras[3];
-		if (DNI == dniAgresor) {
-			Estado = EstadoUsuario;
-			break;
-		}
-		else {
-			Estado = "No Bloqueado";
-		}
+
+	SqlCommand^ objQuery2 = gcnew SqlCommand();
+	objQuery2->Connection = this->objConexion;
+	objQuery2->CommandText = "select * from Quejas where DniAgresor='" + DNI + "';";
+	SqlDataReader^ objData2 = objQuery2->ExecuteReader();
+	if (objData2->Read()) {
+		Estado = safe_cast<String^>(objData2["Estado"]);
 	}
+	else {
+		Estado = "No Bloqueado";
+	}
+	objData2->Close();
 	return Estado;
 }
--- SistemaClasesParticularesController/LogeoController.h
+++ SistemaClasesParticularesController/LogeoController.h
@@ -1,18 +1,25 @@
 #pragma once
 
+using namespace System::Collections::Generic;
+using namespace SistemaClasesParticularesModel;
+using namespace System;
+using namespace System::Data;
+using namespace System::Data::Common;
+using namespace System::Data::SqlClient;
+
+
 namespace SistemaClasesParticularesController {
 
-	using namespace System::Collections::Generic;
-	using namespace SistemaClasesParticularesModel;
-	using namespace System;
 
 	public ref class LogeoController {
 
 	private:
 		//List < Persona^ >^ listaPersonas;
 		int Existe;
-		String^ Verificacion;
+		String^ EstadoCV;
 		String^ Estado;
+		String^ EstadoUsuario;
+		SqlConnection^ objConexion;
 		//String^ usuarioAlumno;
 
 	public:
@@ -21,6 +28,8 @@ namespace SistemaClasesParticularesController {
 		int VerificarSiUsuarioExiste(String^ textBox1, String^ textBox2);
 		//String^ VerificarValidacionCV(String^ DniProfesor);
 		//String^ obtenerUsuario();
+		void AbrirConexion();
+		void CerrarConexion();
 		String^ VerificarValidacionCV(String^ DNI);
 		String^ VerificarEstadoUsuario(String^ DNI); // Se verifica si está bloqueado o no
 	};
