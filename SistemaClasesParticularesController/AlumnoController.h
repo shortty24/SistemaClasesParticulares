0771d23838821a74cd4bf5317d3d49536b1797e6
Cristhian_MC <kevin_mallqui_01@hotmail.com>
2021-07-04 19:00:24 -0500

    Se añadieron las funciones correspondientes a la lectura en bases de dato de AlumnoController.

--- SistemaClasesParticularesController/AlumnoController.h
+++ SistemaClasesParticularesController/AlumnoController.h
@@ -4,13 +4,18 @@ namespace SistemaClasesParticularesController {
 	using namespace System::Collections::Generic;
 	using namespace SistemaClasesParticularesModel;
 	using namespace System;
+	using namespace System::Data::SqlClient;
 	public ref class AlumnoController{
 		private:
 			List < Alumno^ >^ listaAlumnos;
+			SqlConnection^ objConexion;
 		public:
 			AlumnoController();
 			Alumno^ buscaAlumno(String^ usuarioAlumno);
 			Alumno^ buscaAlumnoxDNI(String^ dniAlumno);
-			/*hola*/
+			void AbrirConexion();
+			void CerrarConexion();
+			Alumno^ buscaAlumnoBD(String^ usuarioAlumno);
+			Alumno^ buscaAlumnoxDNI_BD(String^ dniAlumno);
 	};
 }
