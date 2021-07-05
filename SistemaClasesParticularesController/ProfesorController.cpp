b3c29dedae31caf54489aea95841faf251a7d98c
Sin Nombres Sin Apellidos <Nando_ax5@live.com>
2021-07-05 04:18:13 -0500

    Implementé el código para que se muestre los datos del perfil del profesor consultando con la base de datos

--- SistemaClasesParticularesController/ProfesorController.cpp
+++ SistemaClasesParticularesController/ProfesorController.cpp
@@ -6,25 +6,34 @@ using namespace System::IO;
 
 ProfesorController::ProfesorController() {
 	this->listaProfesores = gcnew List<Profesor^>();
+	this->objConexion = gcnew SqlConnection();
+}
+void ProfesorController::AbrirConexion() {
+	/*La cadena de conexión está compuesto de: Servidor de BD, nombre de BD, usuario BD, password BD*/
+	this->objConexion->ConnectionString = "Server=200.16.7.140;DataBase=a20165855;User ID=a20165855;Password=h7b3EJcM;";
+	this->objConexion->Open();/*Ya establecí conexión con la BD*/
+}
+void ProfesorController::CerrarConexion() {
+	this->objConexion->Close();
 }
 Profesor^ ProfesorController::buscaProfesor(String^ usuarioProfesor) {
+	AbrirConexion();
 	Profesor^ objProfesorEncontrado;
-	array<String^>^ lineas = File::ReadAllLines("Personas.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaProfesor in lineas) {
-		array<String^>^ palabras = lineaProfesor->Split(separadores->ToCharArray());
-		String^ dni = palabras[1];
-		String^ usuario = palabras[2];
-		String^ contrasenha = palabras[3];
-		String^ apellidoPaterno = palabras[4];
-		String^ apellidoMaterno = palabras[5];
-		String^ nombre = palabras[6];
-
-		if (usuario == usuarioProfesor) {
-			objProfesorEncontrado = gcnew Profesor(dni, usuario, contrasenha, apellidoPaterno, apellidoMaterno, nombre);
-			break;
-		}
+	SqlCommand^ objQuery = gcnew SqlCommand();
+	objQuery->Connection = this->objConexion;
+	objQuery->CommandText = "select * from Personas where Usuario='" + usuarioProfesor + "';";
+	SqlDataReader^ objData = objQuery->ExecuteReader();
+	if (objData->Read()) {
+		String^ dni = safe_cast<String^>(objData["DNI"]);
+		String^ usuario = safe_cast<String^>(objData["Usuario"]);
+		String^ contrasenha = safe_cast<String^>(objData["Contrasenha"]);
+		String^ apellidoPaterno = safe_cast<String^>(objData["ApellidoPaterno"]);
+		String^ apellidoMaterno = safe_cast<String^>(objData["ApellidoMaterno"]);
+		String^ nombre = safe_cast<String^>(objData["Nombre"]);
+		objData->Close();
+		objProfesorEncontrado = gcnew Profesor(dni, usuario, contrasenha, apellidoPaterno, apellidoMaterno, nombre);
 	}
+	CerrarConexion();
 	return objProfesorEncontrado;
 }
 String^ ProfesorController::obtenerUsuarioxNombreCompleto(String^ nombreCompleto) {
--- SistemaClasesParticularesController/ProfesorController.h
+++ SistemaClasesParticularesController/ProfesorController.h
@@ -5,16 +5,20 @@ namespace SistemaClasesParticularesController {
 	using namespace System::Collections::Generic;
 	using namespace SistemaClasesParticularesModel;
 	using namespace System;
+	using namespace System::Data::SqlClient;
 
 	public ref class ProfesorController {
 		private:
 			List<Profesor^>^ listaProfesores;
+			SqlConnection^ objConexion;
 		public:
 			ProfesorController();
 			Profesor^ buscaProfesor(String^ usuarioProfesor);
 			String^ obtenerUsuarioxNombreCompleto(String^ nombreCompleto);
 			String^ obtenerdnixNombreCompleto(String^ nombreCompleto);
 			Profesor^ buscaProfesorxDNI(String^ dniProfe);
+			void AbrirConexion();
+			void CerrarConexion();
 
 
 	};
